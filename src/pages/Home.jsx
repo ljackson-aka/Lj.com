7ddbbf1cae906ce19904a0c294dc536299bef557
ljackson-aka <63591083+ljackson-aka@users.noreply.github.com>
2025-02-28 10:11:16 -0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,13 @@ import ApplicationTracker from "../components/ApplicationTracker";
 const Home = () => {
   return (
     <div className="home-container">
-      <ApplicationTracker />  {/* Tracker stays in the top-right */}
-      <div className="home-content">
+      <ApplicationTracker /> {/* Stays in the top-right */}
+      
+      <div className="header">
         <h1>Landrick Jackson | Full-Stack AWS Developer</h1>
+      </div>
+
+      <div className="home-content">
         <p>Welcome to Lj.com</p>
       </div>
     </div>
